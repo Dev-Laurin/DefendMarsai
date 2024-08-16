63241c871b5262ca93c07f91168f803176742ec8
Dev-Laurin <lmckenna69@protonmail.com>
2024-08-16 14:01:47 -0800

    cutscene loads backgrounds and character sprites

--- Defend Marsai/Assets/Scripts/Dialogue/DialogueManager.cs
+++ Defend Marsai/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour
 {
     [SerializeField] public GameObject leftPanel; 
     [SerializeField] public GameObject rightPanel; 
+    [SerializeField] public GameObject leftPortrait; 
+    [SerializeField] public GameObject rightPortrait;
+    [SerializeField] public GameObject background; 
     private JsonReader _jsonReader = new JsonReader(); 
     private bool cutscenePlaying = false; 
     private int _index = 0; 
     private bool _left = true; 
     private Conversation _conversation;
+    private Cutscene _cutscene; 
 
     // Start is called before the first frame update
     void Start()
@@ -18,42 +23,47 @@ public class DialogueManager : MonoBehaviour
         LoadCutscene("Cutscenes/tutorial"); 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(cutscenePlaying){
-            //user input
-        }
-    }
-
     public void LoadCutscene(string filename){
         _jsonReader.LoadJsonFile(filename); 
-        _conversation = _jsonReader.DeserializeCutscene().conversation; 
+        _cutscene = _jsonReader.DeserializeCutscene(); 
+        _conversation = _cutscene.conversation; 
 
         cutscenePlaying = true; 
+        UpdateBackground(_cutscene.scene, background); 
         StartCoroutine(ContinueCutscene()); 
     }
 
     private IEnumerator ContinueCutscene(){
         if(_index >= _conversation.messages.Length){
-            Debug.Log("Conversation finished"); 
             cutscenePlaying = false; 
             yield break;
         }
         if(_left){
             DisplayDialogue(_conversation.messages[_index].message, leftPanel); 
+            UpdatePortrait(_conversation.messages[_index].sprite, _conversation.messages[_index].name, leftPortrait); 
         }
         else{
             DisplayDialogue(_conversation.messages[_index].message, rightPanel); 
+            UpdatePortrait(_conversation.messages[_index].sprite, _conversation.messages[_index].name, rightPortrait); 
         }
         
         _index += 1; 
         _left = !_left; 
-        yield return new WaitForSeconds(4);
+        yield return new WaitForSeconds(3);
         yield return StartCoroutine(ContinueCutscene()); 
     }
 
     private void DisplayDialogue(string text, GameObject textObj){
         textObj.GetComponent<TMPro.TextMeshProUGUI>().text = text;
+    }
+
+    private void UpdatePortrait(string sprite, string characterName, GameObject portrait){
+        portrait.GetComponent<Image>().overrideSprite = Resources.Load<Sprite>("Sprites/" + characterName + "/" + sprite); 
+    }
+
+    private void UpdateBackground(string sprite, GameObject background){
+        Debug.Log("Updating Background"); 
+        Debug.Log(sprite); 
+        background.GetComponent<Image>().overrideSprite = Resources.Load<Sprite>("Backgrounds/" + sprite); 
     }
 }
